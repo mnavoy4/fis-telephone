67b0fcf62ac96bbf350ddc032dc135eb5d140dc4
mnavoy4 <mnavoy4@gmail.com>
2020-07-21 08:36:08 -0600

    completed assessment

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,22 @@
 //your code here
+const telephoneUrl = 'https://telephone-assessment.herokuapp.com/api/v1/contacts';
+const charactersUl = document.querySelector('#characters');
+
+function fetchCharacters(){
+  fetch(telephoneUrl)
+    .then(response => response.json())
+    .then(response => renderCharacters(response.data))
+}
+
+function renderCharacters(characters){
+  characters.forEach(character => {
+    let newLi = document.createElement("li");
+    newLi.innerHTML = `<img src=${character.imageURL}>
+                      <span>Name: ${character.name}, Phone Number: ${character.phone}</span>
+                      <p>${character.message}</p>
+                      <a href=./contact.html>Leave ${character.name} a message</a>`;
+    charactersUl.append(newLi);
+  })
+}
+
+document.addEventListener("DOMContentLoaded", fetchCharacters());
